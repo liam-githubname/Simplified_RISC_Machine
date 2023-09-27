d248b7a9f809c7587692ab2c2e90e4b47f2aec03
liam harvell <liam@harvell.us>
2023-09-26 22:23:07 -0400

    added data_structure header

---
+++ vm_mem.h
@@ -0,0 +1,14 @@
+#include "instruction.h"
+#include "machine_types.h"
+
+#define MEMORY_SIZE_IN_BYTES (65536 - BYTES_PER_WORD)
+#define MEMORY_SIZE_IN_WORDS (MEMORY_SIZE_IN_BYTES / BYTES_PER_WORD)
+
+
+static union mem_u {
+
+  byte_type bytes[MEMORY_SIZE_IN_BYTES];
+  word_type words[MEMORY_SIZE_IN_WORDS];
+  bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
+
+} memory;
