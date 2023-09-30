9ce172dd81cd27ea4254b3bfd32acf879e08e5ec
Paul <146044730+Paulseit@users.noreply.github.com>
2023-09-30 13:07:21 -0400

    Add files via upload
    I got HI and Low printing. we need formatting | syscall case | error system call

--- vm_mem.c
+++ vm_mem.c
@@ -15,9 +15,12 @@ void print_cur_register(word_type registers[32], BOFHeader header, bin_instr_t i
 
 
   printf("     PC: %d", header.text_start_address);
+  if (HI > 0 || LO >0){
+    printf("	       HI: %d	      LO: %d",HI,LO);
+  }
   for(int i = 0; i < 32; i++) {
     if (i % 6 == 0) printf("\n");
-    printf("GPR[%s]: %d\t", regname_get(i), registers[i]);
+    printf("GPR[%s]: %d   	", regname_get(i), registers[i]);
   }
 
   //TODO: fix the printing of the words that exist for the program
@@ -37,7 +40,7 @@ void print_cur_register(word_type registers[32], BOFHeader header, bin_instr_t i
   // printf("\n%d: %d\t...", registers[28], memory.bytes[registers[28]/BYTES_PER_WORD]);
 
 
-  printf("\n     %d: %d	...", registers[29], memory.bytes[registers[29]/BYTES_PER_WORD]);
+  printf("\n    %d: %d	...", registers[29], memory.bytes[registers[29]/BYTES_PER_WORD]);
   printf("\n==> addr:\t%d %s\n", header.text_start_address, instruction_assembly_form(instruction));
 
 
