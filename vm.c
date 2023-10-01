fb551e4ebab3cbe700e48f6ad4bfe5fc61841e76
Paul <146044730+Paulseit@users.noreply.github.com>
2023-10-01 01:06:18 -0400

    Add files via upload
    fix

--- vm.c
+++ vm.c
@@ -219,11 +219,13 @@ int main(int argc, char *argv[]) {
               break;
             case 43:  //SW
 
-              if(memory.instrs[i].immed.rs == 29) // this is for the $SP
+              if(memory.instrs[i].immed.rs == 29){// this is for the $SP
                 // TODO IMPORTANT. WE have to figure out how to add it for test7
                 // due to it adding words to it
                 // still trying to figure it out
-              
+
+                memory.words[(registers[29] + machine_types_formOffset(memory.instrs[i].immed.immed))/4] = registers[memory.instrs[i].immed.rt]; 
+              }
               if(memory.instrs[i].immed.rs == 28) // this is for the $gp
               memory.words[(header.text_length/4) + machine_types_formOffset(memory.instrs[i].immed.immed)/4] = registers[memory.instrs[i].immed.rt];
               break;
